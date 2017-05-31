e2d66585449621185e3f9c9107437f8800d5d612
KOBAYASHI Shigeru <shigeru.kb@gmail.com>
2017-06-01 03:56:26 +0900

    Restore var name
    $EMACS may be used inside src/emacs/Makefile

--- .ci/emacs.mk
+++ .ci/emacs.mk
@@ -1,10 +1,10 @@
 # Build Emacs recipe
-# Usage: make -f emacs.mk [NAME=emacs-25.1] [PREFIX=/usr/local]
+# Usage: make -f emacs.mk [EMACS_NAME=emacs-25.1] [PREFIX=/usr/local]
 
 PREFIX:=${HOME}/opt
 
-EMACS:=emacs-24.4
-EMACS_ARCHIVE=${EMACS}.tar.xz
+EMACS_NAME:=emacs-24.4
+EMACS_ARCHIVE=${EMACS_NAME}.tar.xz
 EMACS_ARCHIVE_URL=http://ftpmirror.gnu.org/emacs/${EMACS_ARCHIVE}
 
 default: build install
@@ -14,10 +14,10 @@ ${EMACS_ARCHIVE}:
 
 build: ${EMACS_ARCHIVE}
 	tar xf ${EMACS_ARCHIVE}
-	cd ${EMACS} && ./configure --without-all --without-x --prefix=${PREFIX}
-	cd ${EMACS} && make -j9
+	cd ${EMACS_NAME} && ./configure --without-all --without-x --prefix=${PREFIX}
+	cd ${EMACS_NAME} && make -j9
 
 install:
-	cd ${EMACS} && make install-strip
+	cd ${EMACS_NAME} && make install-strip
 
 .PHONY: build install
